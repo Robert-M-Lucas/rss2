fe1a81be659908dc2efe2610945abca70f450960
= <=>
2025-12-03 14:59:37 +0000

    Update

--- build.rs
+++ build.rs
@@ -1,6 +1,26 @@
+use std::fs;
+
+#[cfg(windows)]
+fn is_nixos() -> bool { false }
+
+#[cfg(unix)]
+fn is_nixos() -> bool {
+    let Ok(content) = fs::read_to_string("/etc/os-release") else { return false; };
+
+
+    for line in content.lines() {
+        if let Some(rest) = line.strip_prefix("ID=") {
+            return rest.trim() == "nixos";
+        }
+    }
+
+    return false;
+}
+
 fn main() {
     println!(
-        "cargo:rustc-env=TARGET={}",
-        build_target::target().triple
+        "cargo:rustc-env=TARGET={}{}",
+        build_target::target().triple,
+        if is_nixos() { "_nixos" } else { "" } // Make nixos a different triple - it links to store paths which doesn't work on other distros
     );
 }
